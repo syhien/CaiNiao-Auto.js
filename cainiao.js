47136a1db74d8661f265e06a44840f2a6c103795
syhien <syhien@outlook.at>
2022-05-27 19:59:26 +0800

    增加任务：天天领红包

--- cainiao.js
+++ cainiao.js
@@ -5,18 +5,39 @@ enterJobPage();
 
 while (1) {
     log("准备做任务");
-    nextMission = text("去完成").findOne();
-    nextMission.click();
-    log("点击去完成");
-    //等待加载完成任务页面，可能要调整
-    sleep(1000);
-    if (text("滑动开始计时").exists()) {
+
+    nextMissionButton = text("去完成").findOne();
+    // 获取任务描述
+    missionDesc = nextMissionButton.parent().parent().child(1).child(0).child(0).text();
+    log("任务描述：" + missionDesc);
+
+    // “浏览主会场”开头任务
+    if (missionDesc.startsWith("浏览主会场")) {
+        log("主会场浏览任务");
+        nextMissionButton.click();
+        sleep(1000);
         log("滑动开始计时");
         swipe(35 + random(10, 20), 1828 + random(10, 20), 35 + random(10, 20), 978 + random(10, 20), 1000 + random(100, 200));
         swipe(35 + random(10, 20), 1828 + random(10, 20), 35 + random(10, 20), 978 + random(10, 20), 1000 + random(100, 200));
         swipe(35 + random(10, 20), 1828 + random(10, 20), 35 + random(10, 20), 978 + random(10, 20), 1000 + random(100, 200));
         sleep(35000);
 
+        back();
+        enterJobPage();
+        continue;
+    }
+
+    // "浏览天天领红包"开头任务
+    if (missionDesc.startsWith("浏览天天领红包")) {
+        log("天天领红包浏览任务");
+        nextMissionButton.click();
+        if (text("x").exists()) {
+            text("x").click();
+            sleep(500);
+        }
+        sleep(3000);
+        sleep(21000);
+        back();
         back();
         enterJobPage();
         continue;
